2e96a0e4efd1bc0697d6bbc33732951c4234a957
Rodrigo <snowitxc@gmail.com>
2023-05-13 22:49:42 -0300

    add cron funciton

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -204,7 +204,7 @@ class UsuarioController extends Controller
         return $this->createNewToken($token);
         
 
-    }
+    }  
 
     function editMeInfo(Request $request){
         $user = auth()->user();
